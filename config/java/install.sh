8fb0309d22f66abdba709be6a9fb23d4cba0d0da
lunzhoufei <lunzhoufei@gmail.com>
2022-03-13 01:19:11 -0800

    add java env installation with sdkman

--- config/java/install.sh
+++ config/java/install.sh
@@ -1,15 +1,38 @@
 #!/usr/bin/env bash
 
-source $LIB/colorecho.sh
-green "Start handling java"
+# source $LIB/colorecho.sh
 
-# echo -e '
+echo "Start handling java ===================================================>"
 
-# ###  nodejs configuration
-# export JAVA_HOME=
-# export JAVA_BIN=
 
-# source `brew --prefix nvm`/nvm.sh
-# '
+# Global variables
 
-red "Handling java finished!"
+
+# Local variables
+sdkman_java_version="8.0.302-open"
+sdkman_gradle_version="6.8"
+sdkman_maven_version="3.8.4"
+
+
+function install_sdkman() {
+    if [[ -z $(grep 'sdkman-init.sh' "$HOME/.zshrc") ]]; then
+        echo "Start install sdkman!  ref: https://sdkman.io/install"
+        curl -s "https://get.sdkman.io" | bash
+    fi
+
+    [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
+    echo "$(sdk version)"
+}
+
+
+install_sdkman
+
+sdk install java ${sdkman_java_version}
+sdk install gradle ${sdkman_gradle_version}
+sdk install maven ${sdkman_maven_version}
+
+echo "JAVA_HOME=$JAVA_HOME"
+echo "CLASSPATH=$CLASSPATH"
+
+
+echo "===============================================> Handling java finished!"
